2cf9c061001863492b76ccf8d7e4a93daab98573
nemetvalentin <nemetvalentin7@gmail.com>
2026-06-11 13:33:19 +0200

    Add files via upload

---
+++ WebShop.MVC/Models/StudentViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShop.MVC.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IdNumber { get; set; }
+    }
+}
