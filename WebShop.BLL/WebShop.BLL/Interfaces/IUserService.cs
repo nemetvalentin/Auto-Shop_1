77a8b3bd4936501d3ad7124065ad67bec2d9e416
nemetvalentin <nemetvalentin7@gmail.com>
2026-06-11 13:21:44 +0200

    Delete WebShop.BLL/WebShop.BLL directory

--- WebShop.BLL/WebShop.BLL/Interfaces/IUserService.cs
+++
@@ -1,9 +0,0 @@
-using WebShop.DAL.Models;
-
-namespace WebShop.BLL.Interfaces
-{
-    public interface IUserService
-    {
-        Task<User?> ValidateUserAsync(string username, string password);
-    }
-}
